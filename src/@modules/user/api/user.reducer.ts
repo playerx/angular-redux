488a60791b5532b5ab694463762d42bddd15cf5b
e.zibzibadze <e.zibzibadze@EZEKI-PC>
2017-10-04 10:15:12 +0400

    updated to sandro style

--- src/@modules/user/api/user.reducer.ts
+++ src/@modules/user/api/user.reducer.ts
@@ -10,46 +10,36 @@ import { State, DefaultState } from './model';
 @Injectable()
 export class UserReducer extends ReducerBase<State> {
 
-
 	get initialState() {
 		return DefaultState;
 	}
 
 
-	reducerMapping = {
-		[userActions.Types.LoadList]: this.LoadList,
-		[userActions.Types.LoadById]: this.LoadById,
-		[userActions.Types.LoadListSuccess]: this.LoadListSuccess,
-		[userActions.Types.Add]: this.Add,
-		['PONG']: this.Pong,
-	};
-
-
-	LoadList(state: State, payload: userActions.LoadList) {
+	[userActions.Types.LoadList](state: State, payload: userActions.LoadList) {
 		console.log('LoadList reduced');
 
 		return state;
 	}
 
-	LoadById(state, payload: userActions.LoadById) {
+	[userActions.Types.LoadById](state, payload: userActions.LoadById) {
 		console.log('LoadById reduced');
 
 		return state;
 	}
 
-	LoadListSuccess(state, payload: userActions.LoadListSuccess) {
+	[userActions.Types.LoadListSuccess](state, payload: userActions.LoadListSuccess) {
 		console.log('LoadListSuccess reduced');
 
 		return state;
 	}
 
-	Add(state, payload: userActions.Add) {
+	[userActions.Types.Add](state, payload: userActions.Add) {
 		console.log('Add reduced');
 
 		return state;
 	}
 
-	Pong(state, payload) {
+	['PONG'](state, payload) {
 		console.log('Pong', state, payload);
 
 		return state;
