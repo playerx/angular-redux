fc9af50c2e33596cc7b11127c9f34d76f4fcd3a7
e.zibzibadze <e.zibzibadze@EZEKI-PC>
2017-10-11 14:01:34 +0400

    created main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { HRModule } from '@modules/hr';
 import { StoreModule } from 'angular-redux-dynamic-modules';
 
 import { AppComponent } from './components/app/app.component';
+import { MainComponent } from './components/main/main.component';
 import { Dispatcher } from './components/dispatcher';
 import { appRoutes } from './app.routes';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		MainComponent
 	],
 	imports: [
 		BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
+import { MainComponent } from './components/main/main.component';
+
 export const appRoutes = [
-	{ path: '', redirectTo: '/hr', pathMatch: 'full' },
+	{ path: '', component: MainComponent, pathMatch: 'full' },
 	{ path: 'hr', loadChildren: '@modules/hr/module#HRModule' },
 	{ path: 'user', loadChildren: '@modules/user/module#UserModule' },
 ];
